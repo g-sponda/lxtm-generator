d5c175dfcec24c0a45cbfff89b27fe4d9773eecd
Guilherme Sponda <sponda@archlinux.com.br>
2019-06-17 17:15:39 -0300

    style(code): adjust stdout of application, turn the code more readable              and add a bang comment

--- lxtm-gen.py
+++ lxtm-gen.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 from os import getenv
 import random
 
@@ -5,13 +6,13 @@ LOOKS_X_TO_ME = "Looks {words} To Me"
 LXTM = "L{initials}TM"
 
 GOOD_FILE = getenv('GOOD_FILE') or 'good-synonyms'
-
 good_synonyms = [line.rstrip('\n').title() for line in open(GOOD_FILE)]
 
 rand_num = random.randrange(0, len(good_synonyms))
-
 words = good_synonyms[rand_num].split()
 
-print(LOOKS_X_TO_ME.format(words=' '.join(words)))
+full_phrase = LOOKS_X_TO_ME.format(words=' '.join(words))
+initials = LXTM.format(initials=''.join(
+    [words[i][0] for i in range(len(words))]))
 
-print(LXTM.format(initials=''.join([words[i][0] for i in range(len(words))])))
+print("{initials} ({phrase})".format(initials=initials, phrase=full_phrase))
