053380fb728917a92d9d3f72ae82755f333185d0
Italo Santos <italo.santos@loggi.com>
2019-09-05 10:55:40 -0300

    refact: add support to wordsapi.com, remove good-synonyms and fix random choice of synonyms

--- lxtm-gen.py
+++ lxtm-gen.py
@@ -1,18 +1,38 @@
 #!/usr/bin/python3
-from os import getenv
 import random
+import requests
+from os import getenv
 
-LOOKS_X_TO_ME = "Looks {words} To Me"
-LXTM = "L{initials}TM"
+def get_synonyms(url, headers, timeout=30):
+    req = requests.get(url=url, headers=headers, timeout=timeout)
+    if req.status_code != 200:
+        raise Exception('API error! HTTP status code: {HTTP_STATUS}'.format(HTTP_STATUS=req.status_code))    
+    return req.json().get('synonyms')
 
-GOOD_FILE = getenv('GOOD_FILE') or 'good-synonyms'
-good_synonyms = [line.rstrip('\n').title() for line in open(GOOD_FILE)]
 
-rand_num = random.randrange(0, len(good_synonyms))
-words = good_synonyms[rand_num].split()
+DEFAULT_GOOD_SYNONYMS = ['acceptable','ace','admirable','agreeable','awesome','brill','brilliant','champion','class','commitable','cool','distinguished','divine','eminent','excellent','exceptional','exquisite','fab','fantastic','fine','fine informalsuper','first-class','first-rate','glorious','good','gorgeous','grand','gratifiable','great','heroic','honorable','impressive','magic','magnificent','marvelous','matchless','mind-blowing','nice','of high quality','of the highest quality','of the highest standard','out of this world','outstanding','peerless','perfect','positive','precious','preeminent','premium','prime','proud','quite good','rare','remarkable','renowned','resplendent','royal','satisfactory','smashing','splendid','splendiferous','splendorous','sterling','stupendou','sublime','superb','supercalifragilisticexpialidocious','superior','superlative','supreme','terrific','tip-top','too good to be true','top-notch','transcendent','tremendous','unparalleled','valuable','very good','wicked','wonderful','worthy']
+DEFAULT_WORD = "good"
+RAPIDAPI_TOKEN = "a97b4721a8msh275edcdf05b4f96p1bdd5cjsnf7ec84641ff2"
+WORDSAPI_URL = "https://wordsapiv1.p.mashape.com/words/{word}/synonyms".format(word=DEFAULT_WORD)
+HEADERS = {
+    "X-Mashape-Key": RAPIDAPI_TOKEN,
+    "Accept": "application/json"
+}
 
-full_phrase = LOOKS_X_TO_ME.format(words=' '.join(words))
-initials = LXTM.format(initials=''.join(
-    [words[i][0] for i in range(len(words))]))
+LOOKS_X_TO_ME = "Looks {word} To Me"
+LXTM = "L{initial}TM"
 
-print("{initials} ({phrase})".format(initials=initials, phrase=full_phrase))
+try:
+    good_synonyms = get_synonyms(WORDSAPI_URL, headers=HEADERS)
+    if good_synonyms:
+        word = random.choice(good_synonyms)
+    else:
+        word = random.choice(DEFAULT_GOOD_SYNONYMS)
+except Exception as e:
+    word = DEFAULT_WORD
+    print(e)
+
+full_phrase = LOOKS_X_TO_ME.format(word=''.join(word))
+initial = LXTM.format(initial=''.join(word[0].upper()))
+
+print("{initial} ({phrase})".format(initial=initial.upper(), phrase=full_phrase.title()))
